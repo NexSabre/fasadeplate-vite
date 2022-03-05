d0fc43c3afb01b675f2544d55dc59635a2444b22
Nex Sabre <NexSabre@users.noreply.github.com>
2022-03-05 11:25:19 +0100

    .

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,34 +1,14 @@
 <template>
   <div>
     <v-container>
-      <v-card
-        class="mx-auto"
-        variant="outlined"
-        @click="
-          {
-            {
-              this.$router.push('/diagonal');
-            }
-          }
-        "
-      >
+      <v-card class="mx-auto" variant="outlined" @click="goTo('diagonal')">
         <v-card-header-text>Diagonal</v-card-header-text>
         <v-card-subtitle
           >calculate diagonal base on the short and long edge</v-card-subtitle
         >
       </v-card>
       <br />
-      <v-card
-        class="mx-auto"
-        variant="outlined"
-        @click="
-          {
-            {
-              this.$router.push('/vertical');
-            }
-          }
-        "
-      >
+      <v-card class="mx-auto" variant="outlined" @click="goTo('vertical')">
         <v-card-header-text>Vertical calculator</v-card-header-text>
         <v-card-subtitle>
           Calculate how many desk you need for wall
@@ -43,6 +23,11 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "HomePage",
+  methods: {
+    goTo(pageName: string): any {
+      this.$router.push(`/${pageName}`);
+    },
+  },
   data() {
     return {
       test: 0,
