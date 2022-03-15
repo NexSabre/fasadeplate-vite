56495715da841ded3d87b2dc3c8c9ca070be6b3d
Nex Sabre <NexSabre@users.noreply.github.com>
2022-03-15 21:22:26 +0100

    .

--- tests/unit/core/SingleBoardCacl.test.ts
+++ tests/unit/core/SingleBoardCacl.test.ts
@@ -1,6 +1,6 @@
 import SingleBoardCalc from "../../../src/core/SingleBoardCalc";
 
-describe("Board on Board", () => {
+describe("SingleBoardCalc", () => {
   it("should calculate properly", () => {
     let sbc = new SingleBoardCalc(14.8, 345.5);
 
@@ -10,5 +10,6 @@ describe("Board on Board", () => {
     expect(sbc.coverate.toFixed(4)).toBe("340.4000");
     expect(sbc.countFullSizeBoards).toBe(23);
     expect(sbc.missingPart.toFixed(4)).toBe("5.1000");
+    expect(sbc.clippings).toBeTruthy();
   });
 });
